26e590ee0c04c40a32d6415327606ffd59ecf558
kevinkowalew <9099021+kevinkowalew@users.noreply.github.com>
2019-11-16 23:12:51 -0600

    tests deploy pipeline for src change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Oh boy vim is swell.
+          Oh boy vim was swell.
         </p>
         <a
           className="App-link"
